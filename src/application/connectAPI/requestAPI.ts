83339db1f43181745cee419f97ecdf77328869a5
WinTDemi <120547068+WinTDemi@users.noreply.github.com>
2024-11-04 18:04:32 +0700

    Fix Review code 4/11/2024

--- src/application/connectAPI/requestAPI.ts
+++ src/application/connectAPI/requestAPI.ts
@@ -1,4 +1,6 @@
 import axios, { AxiosRequestHeaders, Method } from "axios";
+import { handleSuccessAPI } from "./handleSuccessAPI";
+import { handleErrorAxiosAPI } from "./handleErrorAxiosAPI";
 
 interface RequestAPIParams {
     path: string;
@@ -22,74 +24,12 @@ const requestAPI = async <T>({ path, method, headers = {} as AxiosRequestHeaders
             timeout: timeout,
         });
 
-        switch (response.status) {
-            case 201:
-                alert('201 created success');
-                break;
-            case 202:
-                alert('202 accepted success');
-                break;
-            case 203:
-                alert('203 non-authoritative information success');
-                break;
-            case 204:
-                alert('204 no content success');
-                break;
-            case 205:
-                alert('205 reset content success');
-                break;
-            case 206:
-                alert('206 partial content success');
-                break;
-            case 207:
-                alert('207 multi-status success');
-                break;
-            case 208:
-                alert('208 already reported success');
-                break;
-            default:
-                break;
-        }
+        handleSuccessAPI(response?.status);
 
         // console.log(response)
         return response.data as T;
     } catch (error) {
-        if (axios.isAxiosError(error)) {
-            // try good but error response from server
-            // console.error('Error in axios error');
-            // console.error(error.response?.data);
-            // throw error message from server or default message back to TanStack Query
-            // Ném lỗi để TanStack Query có thể nhận diện
-            switch (error.response?.status) {
-                case 400:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || "Bad Request");
-                case 401:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || "Unauthorized");
-                case 403:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || "Forbidden");
-                case 404:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || "Not Found haha");
-                case 500:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || "Internal Server Error");
-                case 502:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || "Bad Gateway");
-                case 503:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || "Service Unavailable");
-                case 504:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || "Gateway Timeout");
-                case 422:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || "Unprocessable Entity");
-                case 429:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || "Too Many Requests");
-                case 409:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || "Conflict");
-                default:
-                    throw new Error(error.response?.status + ' ' + error.response?.data?.message || error.message || "Unknown API error");
-            }
-        }
-        // console.log('Error out axios error');
-        console.error('Unexpected Error:', error);
-        throw new Error("An unexpected error occurred");
+        throw new Error(handleErrorAxiosAPI(error));
     }
 };
 
